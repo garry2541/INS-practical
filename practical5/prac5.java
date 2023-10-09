3af7af3ddf79799a9cd4443d0fd7785770f33f85
Agent 355 <147298473+ggdadda@users.noreply.github.com>
2023-10-09 20:19:20 +0530

    prac5.java file

---
+++ practical5/prac5.java
@@ -0,0 +1,43 @@
+package com.mycompany.prac5;
+import java.io.*;
+import java.security.*;
+
+public class Prac5 {
+
+    public static void main(String[] args) {
+         if(args.length==1){
+            System.out.println("Usage: nameOfFileToSign");
+        }
+        else try{
+            KeyPairGenerator keyGen=KeyPairGenerator.getInstance("DSA","SUN");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG","SUN");
+            keyGen.initialize(1024, random);
+            KeyPair pair=keyGen.generateKeyPair();
+            PrivateKey priv=pair.getPrivate();
+            PublicKey pub=pair.getPublic();
+            Signature dsa=Signature.getInstance("SHA1withDSA","SUN");
+            dsa.initSign(priv);
+            FileInputStream fis=new FileInputStream("D:\\My things\\TYCS\\Study material\\digital signature\\digital.txt");
+            BufferedInputStream bis=new BufferedInputStream(fis);
+            byte[] buffer=new byte[1024];
+            int len;
+            while(bis.available()!=0){
+                len=bis.read(buffer);
+                dsa.update(buffer,0,len);
+            }
+            bis.close();
+            byte[] realSig=dsa.sign();
+            FileOutputStream fos=new FileOutputStream("D:\\My things\\TYCS\\Study material\\digital signature\\signature.txt");
+            fos.write(realSig);
+            fos.close();
+            byte[] key=pub.getEncoded();
+            FileOutputStream keyfos=new FileOutputStream("D:\\My things\\TYCS\\Study material\\digital signature\\publickey.txt");
+            keyfos.write(key);
+            keyfos.close();
+        }catch(Exception e){
+            System.out.println("Caught Exception:"+e.toString());
+        }
+    }
+  
+    }
+
