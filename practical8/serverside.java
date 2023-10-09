83df6ce7d2f4fe77e9b720f9dc651b8f1e4d6cee
Agent 355 <147298473+ggdadda@users.noreply.github.com>
2023-10-09 21:03:03 +0530

    java main class file

---
+++ practical8/serverside.java
@@ -0,0 +1,34 @@
+package prac8;
+import java.io.*;
+import java.net.*;
+public class serverside {
+    public static void main(String args[]) throws IOException{
+        Socket socket;
+        ServerSocket serversocket = new ServerSocket(5000);
+        java.io.InputStreamReader insr = null;
+        java.io.OutputStreamWriter oswr = null;
+        java.io.BufferedReader br = null;
+        java.io.BufferedWriter bw = null;
+        while(true){
+            try{
+                socket = serversocket.accept();
+                insr = new InputStreamReader(socket.getInputStream());
+                oswr = new OutputStreamWriter(socket.getOutputStream());
+                br = new BufferedReader(insr);
+                bw = new BufferedWriter(oswr);
+                while(true){
+                    String msgfromclient = br.readLine();
+                    System.out.println(msgfromclient);
+                    bw.write("Message Received");
+                    bw.newLine();
+                    bw.flush();
+                    if(msgfromclient.equalsIgnoreCase("BYE")){
+                        break;
+                    }
+                }
+            }catch(Exception e){
+                System.out.println("Error: " + e);
+            }
+        }
+    }
+}
